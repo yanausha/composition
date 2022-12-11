cd5f2f20a49acba8fa9f2048968f222334be3c7f
Yana Ushakova <junk7day@gmail.com>
2022-12-11 18:05:37 +0300

    add fun startGame()

--- app/src/main/java/com/example/composition/presentation/GameViewModel.kt
+++ app/src/main/java/com/example/composition/presentation/GameViewModel.kt
@@ -2,14 +2,24 @@ package com.example.composition.presentation
 
 import androidx.lifecycle.ViewModel
 import com.example.composition.data.GameRepositoryImpl
+import com.example.composition.domain.entity.GameSettings
+import com.example.composition.domain.entity.Level
 import com.example.composition.domain.usecases.GenerateQuestionUseCase
 import com.example.composition.domain.usecases.GetGameSettingsUseCase
 
 class GameViewModel: ViewModel() {
 
+    private lateinit var gameSettings: GameSettings
+    private lateinit var level: Level
+
     private val repository = GameRepositoryImpl
 
     private val generateQuestionUseCase = GenerateQuestionUseCase(repository)
     private val getGameSettingsUseCase = GetGameSettingsUseCase(repository)
+
+    fun startGame(level: Level) {
+        this.level = level
+        this.gameSettings = getGameSettingsUseCase(level)
+    }
 
 }
