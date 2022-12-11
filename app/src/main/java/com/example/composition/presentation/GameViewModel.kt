d2143f0514bafe22c9be358fd4445293243eaf13
Yana Ushakova <junk7day@gmail.com>
2022-12-11 18:34:19 +0300

    set timer

--- app/src/main/java/com/example/composition/presentation/GameViewModel.kt
+++ app/src/main/java/com/example/composition/presentation/GameViewModel.kt
@@ -1,5 +1,8 @@
 package com.example.composition.presentation
 
+import android.os.CountDownTimer
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.composition.data.GameRepositoryImpl
 import com.example.composition.domain.entity.GameSettings
@@ -7,7 +10,7 @@ import com.example.composition.domain.entity.Level
 import com.example.composition.domain.usecases.GenerateQuestionUseCase
 import com.example.composition.domain.usecases.GetGameSettingsUseCase
 
-class GameViewModel: ViewModel() {
+class GameViewModel : ViewModel() {
 
     private lateinit var gameSettings: GameSettings
     private lateinit var level: Level
@@ -17,9 +20,58 @@ class GameViewModel: ViewModel() {
     private val generateQuestionUseCase = GenerateQuestionUseCase(repository)
     private val getGameSettingsUseCase = GetGameSettingsUseCase(repository)
 
+    private var timer: CountDownTimer? = null
+
+    private val _formattedTimer = MutableLiveData<String>()
+    val formattedTimer: LiveData<String>
+        get() = _formattedTimer
+
     fun startGame(level: Level) {
+        getGameSettings(level)
+        startTimer()
+    }
+
+    private fun getGameSettings(level: Level) {
         this.level = level
         this.gameSettings = getGameSettingsUseCase(level)
+    }
+
+    private fun startTimer() {
+        timer = object : CountDownTimer(
+            gameSettings.gameTimeInSeconds * MILLIS_IN_SECONDS,
+            MILLIS_IN_SECONDS
+        ) {
+            override fun onTick(millisUntilFinished: Long) {
+                _formattedTimer.value = timeFormat(millisUntilFinished)
+            }
+
+            override fun onFinish() {
+                finishGame()
+            }
+        }
+        timer?.start()
+    }
+
+    private fun timeFormat(millisUntilFinished: Long): String {
+        val seconds = millisUntilFinished / MILLIS_IN_SECONDS
+        val minutes = seconds / SECONDS_IN_MINUTES
+        val leftSeconds = seconds - minutes * SECONDS_IN_MINUTES
+        return String.format("%02d:%02d", minutes, leftSeconds)
+    }
+
+    private fun finishGame() {
+
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        timer?.cancel()
+    }
+
+    companion object {
+
+        private const val MILLIS_IN_SECONDS = 1000L
+        private const val SECONDS_IN_MINUTES = 60
     }
 
 }
